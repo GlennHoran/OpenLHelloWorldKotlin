00a882c552fd0560a1f741d4bc07d2d129166fac
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2018-10-22 12:38:51 +0100

    experiementing with runtimeinterface gen

--- src/main/kotlin/com/lmig/griffin/kotlinopenl/rules/main.kt
+++ src/main/kotlin/com/lmig/griffin/kotlinopenl/rules/main.kt
@@ -5,15 +5,27 @@ import org.openl.runtime.EngineFactory
 import com.lmig.griffin.kotlinopenl.model.Case
 import com.lmig.griffin.kotlinopenl.model.Response
 import com.lmig.griffin.kotlinopenl.model.User
+import java.lang.reflect.Method
+import java.util.*
+
 
 class Main {
-    private var instance: IRule? = null
 
-    fun process(aCase: Case, response: Response) {
-        val engineFactory :EngineFactory<IRule> = RulesEngineFactory(javaClass.classLoader
-                .getResource("openltablets/HelloUser1.xls"), IRule::class.java)
-        instance = engineFactory.newEngineInstance()
-        instance!!.helloUser(aCase, response)
+
+    fun callRulesWithGeneratedInterface() {
+        val engineFactory: EngineFactory<IRule> = RulesEngineFactory("/Users/n0326266/Documents/Sandbox/Griffin/KotlinOpenL/src/main/resources/openltablets/HelloUser1.xls")
+
+        val calendar: Calendar = Calendar.getInstance()
+        val hour: Int = calendar.get(Calendar.HOUR_OF_DAY)
+        val rulesFactory = engineFactory.interfaceClass
+        val user = User("Glenn")
+        val aCase = Case(user, hour)
+        try {
+            val method: Method = rulesFactory.getMethod("helloUser", Int.javaClass)
+            println("Executing OpenL Rules")
+            method.invoke(aCase, "")
+        } catch (e: NoSuchMethodException) {
+        }
     }
 
     companion object {
@@ -21,13 +33,34 @@ class Main {
         @JvmStatic
         fun main(args: Array<String>) {
             val rules = Main()
-            val user = User("Glenn")
-            val aCase = Case(user, 12)
-            val response = Response("")
-            rules.process(aCase, response)
+            rules.callRulesWithGeneratedInterface()
         }
     }
 }
+
+
+//class Main {
+//    private var instance: IRule? = null
+//
+//    fun process(aCase: Case, response: Response) {
+//        val engineFactory :EngineFactory<IRule> = RulesEngineFactory(javaClass.classLoader
+//                .getResource("openltablets/HelloUser1.xls"), IRule::class.java)
+//        instance = engineFactory.newEngineInstance()
+//        instance!!.helloUser(aCase, response)
+//    }
+//
+//    companion object {
+//
+//        @JvmStatic
+//        fun main(args: Array<String>) {
+//            val rules = Main()
+//            val user = User("Glenn")
+//            val aCase = Case(user, 12)
+//            val response = Response("")
+//            rules.process(aCase, response)
+//        }
+//    }
+//}
 
 
 
